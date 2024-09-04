2e58fc40d9118f3241c1e7181e5f3a1ddd64f39b
jurie24102 <jurie@kastelo.com>
2024-09-04 12:05:54 +0200

    google anilit script

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,47 @@
-import "@/styles/globals.css";
+// pages/_app.js
+import { useEffect } from 'react';
+import Script from 'next/script';
+import { useRouter } from 'next/router';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag('config', 'G-VS3C2VKRTK', {
+        page_path: url,
+      });
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  return (
+    <>
+      {/* Google Analytics Script */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-VS3C2VKRTK`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-VS3C2VKRTK', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+export default MyApp;
