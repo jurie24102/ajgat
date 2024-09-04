45e0c24256c75fd9bdbb286d43c04c5465448f75
jurie24102 <jurie@kastelo.com>
2024-09-04 12:38:52 +0200

    somewhere

--- src/pages/somewhere.js
+++ src/pages/somewhere.js
@@ -1,13 +1,14 @@
 import Head from "next/head";
+import { Inter } from "next/font/google";
 import { Box, Button, Typography } from "@mui/material";
-import useRouter from "next/router";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const router = useRouter();
   return (
     <>
       <Head>
-        <title>A&JGAT ~ Whabam!</title>
+        <title>Somewhere ~ Whabam!</title>
         <meta name="description" content="Whabam! Analytics!" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
@@ -22,9 +23,9 @@ export default function Home() {
             height: "100vh",
           }}
         >
-          <Typography variant="h2">André & Jurie Google Analytics Test Site</Typography>
+          <Typography variant="h2">Welcome to Somewhere</Typography>
         <Typography variant="h1">Whabam!</Typography>
-        <Button onClick={() => router.push("/somewhere")} variant="contained">Go Somewhere</Button>
+        
         </Box>
     </>
   );
