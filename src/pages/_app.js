59ae79a808f3857da19a72b53a99a354d3369e2a
jurie24102 <jurie@kastelo.com>
2024-09-04 15:37:40 +0200

    GTM

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,3 @@
-// pages/_app.js
 import { useEffect } from 'react';
 import Script from 'next/script';
 import { useRouter } from 'next/router';
@@ -20,6 +19,21 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
+      {/* Google Tag Manager */}
+      <Script
+        id="gtm-head"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id=' + i + dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NVXMBMMC');
+          `,
+        }}
+      />
+      
       {/* Google Analytics Script */}
       <Script
         strategy="afterInteractive"
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,10 +1,18 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html>
       <Head />
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NVXMBMMC"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         <Main />
         <NextScript />
       </body>
