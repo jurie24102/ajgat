6733e5dc58d5aeface01a74349376480fb716d53
jurie24102 <jurie@kastelo.com>
2024-09-04 16:23:30 +0200

    revert

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,21 +1,61 @@
-// pages/_app.js
 import { useEffect } from 'react';
+import Script from 'next/script';
 import { useRouter } from 'next/router';
-import { initGA, logPageView } from '../lib/gtag';
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
   useEffect(() => {
-    initGA();
-    logPageView();
-    router.events.on('routeChangeComplete', logPageView);
+    const handleRouteChange = (url) => {
+      window.gtag('config', 'G-VS3C2VKRTK', {
+        page_path: url,
+      });
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
     return () => {
-      router.events.off('routeChangeComplete', logPageView);
+      router.events.off('routeChangeComplete', handleRouteChange);
     };
   }, [router.events]);
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* Google Tag Manager */}
+      <Script
+        id="gtm-head"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id=' + i + dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NVXMBMMC');
+          `,
+        }}
+      />
+      
+      {/* Google Analytics Script */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-VS3C2VKRTK`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-VS3C2VKRTK', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,74 +1,31 @@
-// pages/index.js
-import { useState } from 'react';
-import { logEvent } from '../lib/gtag';
+import Head from "next/head";
+import { Box, Button, Typography } from "@mui/material";
+import {useRouter} from "next/router";
 
 export default function Home() {
-  const [formData, setFormData] = useState({
-    username: '',
-    email: '',
-    gender: '',
-    phone: '',
-  });
-
-  const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(formData);
-
-    // Log the event in Google Analytics
-    logEvent('User Form', 'Submit');
-
-    // You can add form submission logic here
-  };
-
+  const router = useRouter();
   return (
-    <div style={{ padding: '20px' }}>
-      <h1>User Information Form</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Username:</label>
-          <input
-            type="text"
-            name="username"
-            value={formData.username}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label>Email:</label>
-          <input
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label>Gender:</label>
-          <select name="gender" value={formData.gender} onChange={handleChange}>
-            <option value="">Select Gender</option>
-            <option value="male">Male</option>
-            <option value="female">Female</option>
-            <option value="other">Other</option>
-          </select>
-        </div>
-        <div>
-          <label>Phone:</label>
-          <input
-            type="text"
-            name="phone"
-            value={formData.phone}
-            onChange={handleChange}
-          />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+    <>
+      <Head>
+        <title>A&JGAT ~ Whabam!</title>
+        <meta name="description" content="Whabam! Analytics!" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            width: "100%",
+            height: "100vh",
+          }}
+        >
+          <Typography variant="h2">André & Jurie Google Analytics Test Site</Typography>
+        <Typography variant="h1">Whabam!</Typography>
+        <Button onClick={() => router.push("/somewhere")} variant="contained">Go Somewhere</Button>
+        </Box>
+    </>
   );
 }
