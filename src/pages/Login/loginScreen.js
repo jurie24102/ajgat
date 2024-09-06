d3315e8f2a93f305c608afb8df7ac1a35b6ecb39
jurie24102 <jurie@kastelo.com>
2024-09-06 11:11:11 +0200

    use router fix

--- src/pages/Login/loginScreen.js
+++ src/pages/Login/loginScreen.js
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { useRouter } from 'next/router';
+import useRouter from 'next/router';
 import { Button, Box } from '@mui/material';
 import gaEvent from '@/lib/gaEvent';
 import Head from 'next/head';
