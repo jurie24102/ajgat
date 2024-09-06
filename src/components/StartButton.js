1c44c7bc4e03bcdfba6968b86bb92a07c621d32c
jurie24102 <jurie@kastelo.com>
2024-09-06 09:44:01 +0200

    .

--- src/components/StartButton.js
+++ src/components/StartButton.js
@@ -12,8 +12,17 @@ const StartButton = () => {
       });
     }
   };
+    const handleClick2 = () => {
+    if (typeof window.gtag === 'function') {
+      window.gtag('event', 'Index Start Button Click', {
+        event_category: 'Index Page2',
+        event_label: 'Index Start Button',
+        value: 'Pressed Start'
+      });
+    }
+  };
 
-  return <Button sx={{ mt: 3 }} onClick={handleClick} variant="contained">Start</Button>;
+  return <><Button sx={{ mt: 3 }} onClick={handleClick} variant="contained">Start</Button><Button sx={{ mt: 3 }} onClick={handleClick} variant="contained">Start2</Button></>;
 };
 
 export default StartButton;
