bb64672dd5a9ae862bdf266fd6e6e5a2c6559604
jurie24102 <jurie@kastelo.com>
2024-09-06 10:43:46 +0200

    import fix

--- src/pages/form.js
+++ src/pages/form.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import { Box, Button, Typography } from "@mui/material";
 import {useRouter} from "next/router";
-import ButtonComponent from "@/components/ButtonComponent";
+import UserColor from "@/components/UserColor";
 
 export default function Home() {
   const router = useRouter();
