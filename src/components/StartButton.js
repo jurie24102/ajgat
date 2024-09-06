40baeb456c2531c14b700aefe9560d6f46ecf78f
jurie24102 <jurie@kastelo.com>
2024-09-06 09:52:25 +0200

    .

--- src/components/StartButton.js
+++ src/components/StartButton.js
@@ -1,28 +1,27 @@
 import { Button } from '@mui/material';
-import { useEffect } from 'react';
 
 const StartButton = () => {
 
-  const handleClick = () => {
+  const handleClick = (buttonNum) => {
     if (typeof window.gtag === 'function') {
       window.gtag('event', 'Index Start Button Click', {
-        event_category: 'Index Page',
-        event_label: 'Index Start Button',
-        value: 'Pressed Start'
-      });
-    }
-  };
-    const handleClick2 = () => {
-    if (typeof window.gtag === 'function') {
-      window.gtag('event', 'Index Start Button Click', {
-        event_category: 'Index Page2',
+        event_category: buttonNum === 'button1' ? 'Index Page' : 'Index Page2',
         event_label: 'Index Start Button',
         value: 'Pressed Start'
       });
     }
   };
 
-  return <><Button sx={{ mt: 3 }} onClick={handleClick} variant="contained">Start</Button><Button sx={{ mt: 3 }} onClick={handleClick2} variant="contained">Start2</Button></>;
+  return (
+    <>
+      <Button sx={{ mt: 3 }} onClick={() => handleClick('button1')} variant="contained">
+        Start
+      </Button>
+      <Button sx={{ mt: 3 }} onClick={() => handleClick('button2')} variant="contained">
+        Start2
+      </Button>
+    </>
+  );
 };
 
 export default StartButton;
